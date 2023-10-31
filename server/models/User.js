3af6f7a900fc1502f5ccc25c4e2bc6d6ceddbcb5
PRich57 <pcrichards57@gmail.com>
2023-10-31 11:06:38 -0700

    Create user model

---
+++ server/models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    match: [/(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/, 'Minimum 8 characters, must include lowercase, uppercase, number, and special character']
+  },
+  blurbs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Blurb',
+    },
+  ],
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
