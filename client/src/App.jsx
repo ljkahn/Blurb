b0158efc6bdd1c1640bf0fe845a4b33c493de224
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-31 14:37:28 -0500

    client>utils>auth.js started using setContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,39 @@ import {
 } from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 import {Outlet} from 'react-router-dom';
+//Can we use hash router here for the pages
+//Would need to import all componenets and pages if so 
 
 
 import './App.css' //change to styles folder 
 
 const httpLink = createHttpLink({
-  uri: '/gr'
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+}); 
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 })
 
 function App() {
 
 
   return (
-    <>
+    <ApolloProvider client={client}>
 
-    </>
+    </ApolloProvider>
   )
 }
 
