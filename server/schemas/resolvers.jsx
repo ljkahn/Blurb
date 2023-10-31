12fd4eea65203dae82c82fd0d78c5dc7a82afac1
ljkahn <lia.j.kahn@gmail.com>
2023-10-31 13:00:13 -0600

    created user resolvers

---
+++ server/schemas/resolvers.jsx
@@ -0,0 +1,25 @@
+const { User } = require('../models')
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+
+        users: async (parent, {profileId}) => {
+            return User.findOne({_id: profileId}).populate('blurbs');
+        },
+
+        me: async (parent, args, context) => {
+            if(context.user) {
+                return User.findOne({_id: context.user._id})
+            }
+            throw AuthenticationError;
+        }
+    }
+}
+
+
+
+module.exports = resolvers
