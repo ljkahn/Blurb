0beca8425da59262835897f8160a583cb60dd569
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-31 19:53:34 -0500

    Start profile styling

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Nav from "../components/NavBar.jsx";
 import Photo from "../components/Profile/ProfilePhoto.jsx";
-import Edit from "../components/Profile/Edit.jsx"
+import Edit from "../components/Profile/Edit.jsx";
+import Button from "@mui/material/Button";
+import Container from '@mui/material/Container';
 
 
 function Profile() {
+
+  //use auth to create ternary statement for 
+  //1. whether or not edit profile button shows up
+  //2. what user name to pop up 
+
+  //Use query to display blubrs attatched to one user
   return (
-    <div>Profile</div>
+    <div>
+      <Container>
+      <Photo/>
+      <h1>Lillian Edwards</h1>
+      <h2>lillianedwards</h2>
+      <Button><a class="waves-effect  btn">103 Followers</a></Button>
+      <Button><a class="waves-effect  btn">95 Followers</a></Button>
+      <Button><a class="waves-effect  btn">{Edit}</a></Button>
+      <h3>USER BLURBS - component?</h3>
+      <Nav/>
+      </Container>
+     
+
+    </div>
   )
 }
 
