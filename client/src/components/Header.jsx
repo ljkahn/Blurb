ea607e4b2d350d8f1eed7de2774b5af91ad6c414
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-10-31 18:53:50 -0600

    login page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import "../style/Header.css";
+import Button from "@mui/material/Button";
 
-function header() {
+function Header() {
+  const [value, setValue] = useState(0); // You need to define value and setValue for BottomNavigation
+
   return (
-    <div>header</div>
-  )
+    <div id="header">
+      <div>
+        <BottomNavigation
+          id="searchButton"
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            
+            variant="outlined"
+            icon={<SearchIcon />}
+          />
+        </BottomNavigation>
+      </div>
+      <div className="friendProfile">
+        <div className="friends" />
+        <p>Name</p>
+      </div>
+      <div className="friendProfile">
+        <div className="friends" />
+        <p>Name</p>
+      </div>
+      <div className="friendProfile">
+        <div className="friends" />
+        <p>Name</p>
+      </div>
+      <div className="friendProfile">
+        <div className="friends" />
+        <p>Name</p>
+      </div>
+      <div className="friendProfile">
+        <div className="friends" />
+        <p>Name</p>
+      </div>
+    </div>
+  );
 }
 
-export default header
+export default Header; // Use PascalCase for component names, i.e., Header instead of header
+
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Create from "../components/Login/CreateTab.jsx";
 import LoginTab from "../components/Login/LoginTab.jsx";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import "../style/Login.css";
 
 function Login() {
+  const [tabValue, setTabValue] = useState("login");
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
   return (
-    <div>
-      <h2>Login or Create Tab</h2>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-      >
-        <Tab value="one" label="Item One" />
-        <Tab value="two" label="Item Two" />
-        <Tab value="three" label="Item Three" />
-      </Tabs>
-    </div>
+    <>
+      <div id="login">
+        <div id="logBack">
+          <h2>Login or Create</h2>
+          <Tabs
+            value={tabValue}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="Login or Create Tabs"
+          >
+            <Tab value="login" label="Login" />
+            <Tab value="create" label="Create" />
+          </Tabs>
+
+          {tabValue === "login" && <LoginTab />}
+          {tabValue === "create" && <Create />}
+        </div>
+      </div>
+    </>
   );
 }
 
