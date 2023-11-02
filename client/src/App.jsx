bbd1aac3b82d642002d2b8049dc3ba08c8906e8f
ljkahn <lia.j.kahn@gmail.com>
2023-11-02 12:39:04 -0600

    fixed edit comment resolver

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import React, { useState, useEffect } from "react";
 import "./index.css";
 
+import Notifications from './pages/Notifications'
 import Flame from './pages/Flame'
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -12,9 +14,10 @@ import NavBar from './components/NavBar';
 import Likes from './pages/Likes';
 import CommentPage from "./pages/CommentPage";
 
+
 function App() {
   
-  
+ 
   return (
     <Router>
     <Header/>
@@ -22,6 +25,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/flame" element={<Flame />} />
+        <Route path="/notifications" element={<Notifications />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Error />} />
         <Route path="/likes" element={<Likes />} />
