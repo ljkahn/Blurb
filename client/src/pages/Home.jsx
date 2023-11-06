f049e2ac1dc7816745c7a99e3f2439c361ada3eb
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-11-06 13:35:54 -0700

    styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -48,6 +48,7 @@ function Home() {
               key={i}
               blurbId={blurb._id}
               username={blurb.blurbAuthor.username}
+              profilePic={blurb.blurbAuthor.profile.profilePic}
             >
               {blurb.blurbText}
             </BlurbCard>
