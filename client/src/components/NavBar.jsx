f19174558e1ceba7bc0eae38b00f26eb96638bf3
JMADA257 <jadams1812@hotmail.com>
2023-11-14 14:02:45 -0700

    TestinG

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -75,10 +75,9 @@ function NavBar() {
 
   return (
     <div id="navContain">
-      <Tooltip
-        title="Home page, this is where you can check out other user's blurbs! 👥"
-        enterTouchDelay={0}
-        leaveTouchDelay={2000}
+      <Tooltip title="Home page, this is where you can check out other user's blurbs! 👥"
+      enterTouchDelay={0}
+      leaveTouchDelay={2000}
       >
         <Link to="/home">
           <IconButton>
@@ -89,10 +88,9 @@ function NavBar() {
           </IconButton>
         </Link>
       </Tooltip>
-      <Tooltip
-        title="Flame page, this is where Blurbs with more than 10 likes get promoted to! 🎉"
-        enterTouchDelay={0}
-        leaveTouchDelay={2000}
+      <Tooltip title="Flame page, this is where Blurbs with more than 10 likes get promoted to! 🎉"
+      enterTouchDelay={0}
+      leaveTouchDelay={2000}
       >
         <Link to="/flame">
           <IconButton>
@@ -103,34 +101,25 @@ function NavBar() {
           </IconButton>
         </Link>
       </Tooltip>
-      <Tooltip
-        title="Click this button to post a blurb! 💬 "
+      <Tooltip title="Click this button to post a blurb! 💬 ">
+        <button onClick={openModal} id="addBlurb">
+          <Box
+            component="img"
+            id="logo"
+            sx={{ height: 35 }}
+            alt="Logo"
+            src={logo}
+          />
+        <button onClick={openModal} id="addBlurb"
         enterTouchDelay={0}
         leaveTouchDelay={2000}
-      >
-        <button onClick={openModal} id="addBlurb">
-          <Box
-            component="img"
-            id="logo"
-            sx={{ height: 35 }}
-            alt="Logo"
-            src={logo}
-          />
-        </button>
-        <button onClick={openModal} id="addBlurb">
-          <Box
-            component="img"
-            id="logo"
-            sx={{ height: 35 }}
-            alt="Logo"
-            src={logo}
-          />
+        >
+          <Box component="img" id="logo" sx={{ height: 35 }} alt="Logo" src={logo} />
         </button>
       </Tooltip>
-      <Tooltip
-        title="Notifications page, this is under construction right now! 🚧"
-        enterTouchDelay={0}
-        leaveTouchDelay={2000}
+      <Tooltip title="Notifications page, this is under construction right now! 🚧"
+      enterTouchDelay={0}
+      leaveTouchDelay={2000}
       >
         <Link to="/Likes">
           <IconButton aria-label={notificationsLabel(100)}>
@@ -143,10 +132,9 @@ function NavBar() {
           </IconButton>
         </Link>
       </Tooltip>
-      <Tooltip
-        title="Profile page, this is where you can edit your profile information, and blurbs! ✏️"
-        enterTouchDelay={0}
-        leaveTouchDelay={2000}
+      <Tooltip title="Profile page, this is where you can edit your profile information, and blurbs! ✏️"
+      enterTouchDelay={0}
+      leaveTouchDelay={2000}
       >
         <Link to="/profile">
           <IconButton>
